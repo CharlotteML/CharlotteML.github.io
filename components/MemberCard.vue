5baf4ceab2f6698da5efedc1515934518748db51
TeddyHuang-00 <huang_nan_2019@pku.edu.cn>
2023-08-28 00:25:37 -0400

    feat: :sparkles: Add working memeber page and component

---
+++ components/MemberCard.vue
@@ -0,0 +1,120 @@
+<template>
+  <div
+    class="flex flex-col rounded-lg border border-secondary-100 bg-secondary-100/50 dark:border-secondary-900 dark:bg-secondary-900/30"
+  >
+    <div class="flex flex-col items-center sm:flex-row">
+      <img
+        class="mt-4 h-48 w-48 flex-shrink-0 rounded-full object-cover sm:mt-0 sm:rounded-none sm:rounded-br-lg sm:rounded-tl-lg"
+        :src="person.photo || '/logo-full.svg'"
+        :alt="
+          [person.name.first, person.name.middle, person.name.last].join(' ')
+        "
+      />
+      <div
+        class="flex h-fit flex-col items-center justify-between gap-y-2 p-4 leading-normal sm:h-48 sm:items-start sm:gap-y-0"
+      >
+        <h2 class="text-2xl font-bold tracking-tight">
+          {{
+            [person.name.first, person.name.middle, person.name.last].join(" ")
+          }}
+          -
+          {{ person.position.join(", ") }}
+        </h2>
+        <h3
+          class="text-lg font-semibold tracking-tight text-main-900 dark:text-main-100"
+        ></h3>
+        <p
+          class="flex w-1/2 flex-row flex-wrap justify-center gap-x-1 font-normal text-secondary-800 dark:text-secondary-200 sm:w-full sm:justify-start"
+        >
+          <span> Research: </span>
+          <span v-for="(research, i) in person.research">
+            {{ i == person.research.length ? research : research + ", " }}
+          </span>
+        </p>
+        <div
+          class="flex flex-row flex-wrap text-sm font-light text-secondary-800 dark:text-secondary-200"
+        >
+          <a
+            :href="person.contact.website"
+            v-if="person.contact.website"
+            class="w-full min-w-[50%] text-center sm:w-fit sm:text-left"
+            rel="noopener noreferrer nofollow"
+            target="_blank"
+          >
+            <Icon name="ph:globe" class="mb-0.5 ml-1 h-4 w-4" />
+            {{ person.contact.website }}
+          </a>
+          <a
+            :href="`mailto:${email}`"
+            v-for="email in person.contact.email"
+            class="w-full min-w-[50%] text-center sm:w-fit sm:text-left"
+          >
+            <Icon name="ph:envelope" class="mb-0.5 ml-1 h-4 w-4" />
+            {{ email }}
+          </a>
+          <a
+            :href="`tel:+1-${phone}`"
+            v-for="phone in person.contact.phone"
+            class="w-full min-w-[50%] text-center sm:w-fit sm:text-left"
+          >
+            <Icon name="ph:phone" class="mb-0.5 ml-1 h-4 w-4" />
+            {{ phone }}
+          </a>
+          <span
+            v-if="person.contact.address"
+            class="w-full min-w-[50%] text-center sm:w-fit sm:text-left"
+          >
+            <Icon name="ph:house" class="mb-0.5 ml-1 h-4 w-4" />
+            {{ person.contact.address }}
+          </span>
+        </div>
+      </div>
+    </div>
+    <div class="my-4 h-fit w-full px-6">
+      <p class="text-justify text-lg">
+        <ContentSlot>
+          {{ person.introduction }}
+        </ContentSlot>
+      </p>
+      <div
+        class="my-4 w-full border-t border-dashed"
+        v-if="person.teaching?.length"
+      ></div>
+      <ul v-if="person.teaching?.length">
+        <li v-for="course in person.teaching" class="list-inside list-disc">
+          {{ course }}
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+interface Person {
+  name: {
+    first: string | null;
+    middle: string | null;
+    last: string | null;
+  };
+  position: string[];
+  research: string[];
+  introduction: string;
+  teaching: string[] | null | undefined;
+  contact: {
+    email: string[] | null | undefined;
+    phone: string[] | null | undefined;
+    address: string | null | undefined;
+    website: string | null | undefined;
+  };
+  photo: string | null | undefined;
+}
+
+defineProps({
+  person: {
+    type: Object as PropType<Person>,
+    required: true,
+  },
+});
+</script>
+
+<style scoped></style>
---
+++ pages/member.vue
@@ -0,0 +1,178 @@
+<template>
+  <main class="relative mx-auto mt-0.5 min-h-screen max-w-3xl px-6">
+    <aside
+      class="sticky top-0 z-40 mb-2 h-20 w-full rounded-b-lg bg-secondary-100/30 py-4 backdrop-blur-sm dark:bg-secondary-900/10 lg:fixed lg:right-2 lg:top-1/2 lg:mb-0 lg:h-3/4 lg:w-36 lg:-translate-y-1/2 lg:rounded-lg lg:py-0"
+    >
+      <ul class="flex h-full w-full flex-row gap-1 rounded-xl p-2 lg:flex-col">
+        <li
+          v-if="faculty.length"
+          class="nav rounded-lg border border-primary-800 bg-primary-800/30 text-sm"
+        >
+          Faculty
+        </li>
+        <li
+          v-if="faculty.length"
+          v-for="(f, i) in faculty"
+          :key="'aside' + f.name.first + f.name.middle + f.name.last"
+          class="nav"
+          :class="facFocus[i] ? 'active' : 'inactive'"
+        >
+          <NuxtLink
+            :to="`#${idFromName(f.name.first, f.name.middle, f.name.last)}`"
+          >
+            {{
+              facFocus[i]
+                ? concatName(f.name.first, f.name.middle, f.name.last)
+                : abbrevName(f.name.first, f.name.middle, f.name.last)
+            }}
+          </NuxtLink>
+        </li>
+        <li
+          v-if="student.length"
+          class="nav rounded-lg border border-primary-800 bg-primary-800/30 text-sm"
+        >
+          Student
+        </li>
+        <li
+          v-if="student.length"
+          v-for="(s, i) in student"
+          :key="'aside' + s.name.first + s.name.middle + s.name.last"
+          class="nav"
+          :class="stuFocus[i] ? 'active' : 'inactive'"
+        >
+          <NuxtLink
+            :to="`#${idFromName(s.name.first, s.name.middle, s.name.last)}`"
+          >
+            {{
+              stuFocus[i]
+                ? concatName(s.name.first, s.name.middle, s.name.last)
+                : abbrevName(s.name.first, s.name.middle, s.name.last)
+            }}
+          </NuxtLink>
+        </li>
+        <li
+          v-if="alumni.length"
+          class="nav rounded-lg border border-primary-800 bg-primary-800/30 text-sm"
+        >
+          Alumni
+        </li>
+        <li
+          v-if="alumni.length"
+          v-for="(a, i) in alumni"
+          :key="'aside' + a.name.first + a.name.middle + a.name.last"
+          class="nav"
+          :class="aluFocus[i] ? 'active' : 'inactive'"
+        >
+          <NuxtLink
+            :to="`#${idFromName(a.name.first, a.name.middle, a.name.last)}`"
+          >
+            {{
+              aluFocus[i]
+                ? concatName(a.name.first, a.name.middle, a.name.last)
+                : abbrevName(a.name.first, a.name.middle, a.name.last)
+            }}
+          </NuxtLink>
+        </li>
+      </ul>
+    </aside>
+    <ul class="flex flex-col gap-6 pt-0 lg:pt-6">
+      <li
+        v-for="f in faculty"
+        :key="idFromName(f.name.first, f.name.middle, f.name.last)"
+        :id="idFromName(f.name.first, f.name.middle, f.name.last)"
+        ref="facRefs"
+        class="scroll-m-24"
+      >
+        <MemberCard :person="f" />
+      </li>
+      <li
+        v-for="s in student"
+        :key="idFromName(s.name.first, s.name.middle, s.name.last)"
+        :id="idFromName(s.name.first, s.name.middle, s.name.last)"
+        ref="stuRefs"
+        class="scroll-m-24"
+      >
+        <MemberCard :person="s" />
+      </li>
+      <li
+        v-for="a in alumni"
+        :key="idFromName(a.name.first, a.name.middle, a.name.last)"
+        :id="idFromName(a.name.first, a.name.middle, a.name.last)"
+        ref="aluRefs"
+        class="scroll-m-24"
+      >
+        <MemberCard :person="a" />
+      </li>
+    </ul>
+  </main>
+</template>
+
+<script setup lang="ts">
+const idFromName = (
+  first: string | null,
+  middle: string | null,
+  last: string | null,
+) => {
+  return [first, middle, last].filter((x) => x).join("");
+};
+const concatName = (
+  first: string | null,
+  middle: string | null,
+  last: string | null,
+) => {
+  return [first, middle, last].filter((x) => x).join(" ");
+};
+const abbrevName = (
+  first: string | null,
+  middle: string | null,
+  last: string | null,
+) => {
+  return [first, middle, last]
+    .map((x) => x?.charAt(0))
+    .filter((x) => x)
+    .join(" ");
+};
+
+const { page } = useContent();
+const faculty = page.value.faculty;
+const student = page.value.student;
+const alumni = page.value.alumni;
+
+const stuRefs = ref([] as HTMLElement[]);
+const facRefs = ref([] as HTMLElement[]);
+const aluRefs = ref([] as HTMLElement[]);
+
+const facFocus = ref(faculty.map(() => false));
+const stuFocus = ref(student.map(() => false));
+const aluFocus = ref(alumni.map(() => false));
+
+onMounted(() => {
+  facRefs.value.map((f, i) =>
+    useIntersectionObserver(f, ([{ isIntersecting }]) => {
+      facFocus.value[i] = isIntersecting;
+    }),
+  );
+  stuRefs.value.map((s, i) =>
+    useIntersectionObserver(s, ([{ isIntersecting }]) => {
+      stuFocus.value[i] = isIntersecting;
+    }),
+  );
+  aluRefs.value.map((a, i) =>
+    useIntersectionObserver(a, ([{ isIntersecting }]) => {
+      aluFocus.value[i] = isIntersecting;
+    }),
+  );
+});
+</script>
+
+<style>
+.nav {
+  @apply flex items-center justify-center border-primary-800 p-2 py-4 lg:py-2;
+}
+.inactive {
+  @apply max-w-[10px] flex-grow-0 overflow-hidden border-t text-sm delay-75 lg:max-w-none lg:border-l lg:border-t-0;
+}
+.active {
+  @apply flex-grow border-t-4 delay-75 lg:border-l-4 lg:border-t-0;
+}
+</style>
